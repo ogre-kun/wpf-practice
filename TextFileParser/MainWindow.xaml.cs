928c20401bd34b66550ef3ce1f29e80a2eeaca12
Karol Wojtyla Macaraan <woj.macaraan@outlook.com>
2022-04-02 23:45:01 +0800

    Text file processor sample

---
+++ TextFileParser/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+
+namespace TextFileParser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string FilePath { get; private set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            FilePath = string.Empty;
+        }
+
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                var extension = System.IO.Path.GetExtension(openFileDialog.FileName);
+                if (extension != ".txt")
+                {
+                    MessageBox.Show("Not a text file!");
+                    return;
+                }
+                FilePath = openFileDialog.FileName;
+                PathText.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void Button_Summarize_Click(object sender, RoutedEventArgs e)
+        {
+            if (FilePath == string.Empty)
+            {
+                MessageBox.Show("File path empty!");
+                return;
+            }
+            TextFileProcessor textFile = new TextFileProcessor(FilePath);
+            SummaryText.Text = textFile.GetSummary();
+        }
+    }
+}
---
+++ TextFileParser/TextFileProcessor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace TextFileParser
+{
+    public class TextFileProcessor
+    {
+        private List<string> _contents;
+        
+        public string FilePath { get; private set; }
+
+        public TextFileProcessor(string filepath)
+        {
+            FilePath = filepath;
+            var sr = new StreamReader(filepath);
+            if (sr == null) throw new Exception("File path not found!");
+            _contents = sr.ReadToEnd().Split(' ').ToList();
+        }
+
+        private bool isValidWord(string str)
+        {
+            Regex rx = new Regex("^[a-zA-Z]");
+            return rx.IsMatch(str);
+        }
+
+        public int GetWordCount()
+        {
+
+            return _contents.Where(word => isValidWord(word) == true).ToList().Count;
+        }
+
+        public int GetCapitalCount()
+        {
+            return _contents.Where(word => word.Length != 0 && Char.IsUpper(word[0]) == true).ToList().Count;
+        }
+
+        public int GetLowerCaseCount()
+        {
+            return _contents.Where(word => word.Length != 0 && Char.IsLower(word[0]) == true).ToList().Count;
+        }
+
+        public string GetSummary()
+        {
+            return "Word Count : " + GetWordCount().ToString() + 
+                "\nCapital Words : " + GetCapitalCount().ToString() + 
+                "\nLower Case Words : " + GetLowerCaseCount().ToString();
+        }
+    }
+}
